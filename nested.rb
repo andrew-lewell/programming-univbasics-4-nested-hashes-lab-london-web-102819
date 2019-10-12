9c99f79cc247dc51e8283b8a251204b10b43dbef
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-12 11:54:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
         }
      }
   
-  return programmer_hash.[:dennis_ritchie][:languages].to_s
+  str = programmer_hash.[:dennis_ritchie][:languages]
+  return str.to_s
 end
 
 def adding_matz
